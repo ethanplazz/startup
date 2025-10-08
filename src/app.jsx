f271333ecf9ac3fb90acbe05c891fcee7efd677d
Ethan Plaster <plasterethan@gmail.com>
2025-10-08 16:44:51 -0600

    Added my header and footer to app jsx with a placeholder main as I follow the tutorial. It looks nice on the website.

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header>
+        <h1>Provo Fish Map</h1>
+        <nav>
+          <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="lower.html">Lower Provo</a></li>
+            <li><a href="middle.html">Middle Provo</a></li>
+            <li><a href="upper.html">Upper Provo</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="community.html">Community</a></li>
+          </ul>
+        </nav>
+        <hr />
+      </header>
+
+      <main>App components go here</main>
+
+      <footer>
+        <hr />
+        <span className="text-reset">Ethan Plaster</span>
+        <br />
+        <a href="https://github.com/ethanplazz/startup.git">GitHub</a>
+      </footer>
+    </div>
+  );
 }
