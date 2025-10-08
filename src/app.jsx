e6f2cbf054aa72f25e4b7d8873ed625ec6744297
Ethan Plaster <plasterethan@gmail.com>
2025-10-08 17:05:37 -0600

    Changed a tags to nav links, wrapped everything in a browserrouter, and imported each jsx for each view.

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Lower } from './lower/lower';
+import { Middle } from './middle/middle';
+import { Upper } from './upper/upper';
+import { About } from './about/about';
+import { Community } from './community/community';
 
 export default function App() {
   return (
+    <BrowserRouter>
     <div className="body bg-dark text-light">
       <header>
         <h1>Provo Fish Map</h1>
         <nav>
           <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="lower.html">Lower Provo</a></li>
-            <li><a href="middle.html">Middle Provo</a></li>
-            <li><a href="upper.html">Upper Provo</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="community.html">Community</a></li>
+            <li><NavLink className="nav-link" to="">
+                  Login
+                </NavLink></li>
+            <li><NavLink className="nav-link" to="lower">
+                  Lower Provo
+                </NavLink></li>
+            <li><NavLink className="nav-link" to="middle">
+                  Middle Provo
+                </NavLink></li>
+            <li><NavLink className="nav-link" to="upper">
+                  Upper Provo
+                </NavLink></li>
+            <li><NavLink className="nav-link" to="about">
+                  About
+                </NavLink></li>
+            <li><NavLink className="nav-link" to="community">
+                  Community
+                </NavLink></li>
           </ul>
         </nav>
         <hr />
       </header>
 
-      <main>App components go here</main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/lower' element={<Lower />} />
+        <Route path='/middle' element={<Middle />} />
+        <Route path='/upper' element={<Upper />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/community' element={<Community />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
       <footer>
         <hr />
@@ -29,5 +57,10 @@ export default function App() {
         <a href="https://github.com/ethanplazz/startup.git">GitHub</a>
       </footer>
     </div>
+    </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
