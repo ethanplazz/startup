8a22b93e593677e25cad2d50c89d4aff47e8e3ce
Ethan Plaster <plasterethan@gmail.com>
2025-11-11 16:17:37 -0700

    increased the size limit of images that can be posted.

--- service/index.js
+++ service/index.js
@@ -8,7 +8,8 @@ const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
